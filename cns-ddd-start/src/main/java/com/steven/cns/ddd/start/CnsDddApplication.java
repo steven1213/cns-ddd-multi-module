42723f0b105b350f2a585626091bf887df5e00f4
Steven.cao <steven.cao1213@gmail.com>
2023-03-05 15:37:20 +0800

    feat(first init): first init
    first init

---
+++ cns-ddd-start/src/main/java/com/steven/cns/ddd/start/CnsDddApplication.java
@@ -0,0 +1,17 @@
+package com.steven.cns.ddd.start;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: steven
+ * @date: 2023/3/5 15:31
+ */
+@Slf4j
+@SpringBootApplication
+public class CnsDddApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CnsDddApplication.class, args);
+    }
+}
